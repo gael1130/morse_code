bf9e58cf946326fcd5fb420cd498121674bf6bbd
gael1130 <gael.gendre@gmail.com>
2022-07-20 17:44:01 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+
+message_to_encode = input()
+
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+
+print(message_to_encode.upper())
+morse_list = []
+for letter in message_to_encode.upper():
+    for key, value in MORSE_CODE_DICT.items():
+        if letter == key:
+            morse_list.append(value)
+
+morse_code = "".join(morse_list)
+print(morse_code)
+
+
+# for key, value in MORSE_CODE_DICT.items():
+#     print(key, value)
